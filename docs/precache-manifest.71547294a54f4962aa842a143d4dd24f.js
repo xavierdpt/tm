bd8c9ce653ea39383ca2e364094c0adab5ca2ba6
Xavier Dupont <xavier.dpt@gmail.com>
2018-11-18 16:46:25 +0100

    .

---
+++ docs/precache-manifest.71547294a54f4962aa842a143d4dd24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tm/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74f0e9f3e983c8558e5a",
+    "url": "/tm/static/js/runtime~main.74f0e9f3.js"
+  },
+  {
+    "revision": "3a4e4f0bb11c900d8af5",
+    "url": "/tm/static/js/main.3a4e4f0b.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/tm/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "3a4e4f0bb11c900d8af5",
+    "url": "/tm/static/css/main.55b32ade.chunk.css"
+  },
+  {
+    "revision": "5819d5a7adf62e50093186fd351473f8",
+    "url": "/tm/index.html"
+  }
+];
